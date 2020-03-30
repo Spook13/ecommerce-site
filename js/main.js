7e0a450fce0a0b413f3809f4b20983b205812987
Spook13 <59904473+Spook13@users.noreply.github.com>
2020-03-30 08:19:34 -0500

    Add files via upload
    More editing/ added fontawesome icons and edited it's folder

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// --- Coffee Section ---
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openCoffeeProducts() {
+    document.getElementById("CoffeeProducts").style.width = "100vw";
+    document.getElementById("coffeeSection").style.display = "none";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeCoffeeProducts() {
+    document.getElementById("CoffeeProducts").style.width = "0";
+    document.getElementById("coffeeSection").style.display = "block";
+  }
+
+
+// --- Expresso Section ---
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openExpressoProducts() {
+    document.getElementById("expressoProducts").style.width = "100vw";
+    document.getElementById("expressoSection").style.display = "none";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeExpressoProducts() {
+    document.getElementById("expressoProducts").style.width = "0";
+    document.getElementById("expressoSection").style.display = "block";
+  }
+
+
+
+
